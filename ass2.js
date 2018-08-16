93bd99f6a8bdc3b9b0deb3a2bb5d860d86726831
Raj Carvalho <rajcarvalho@gmail.com>
2018-08-15 21:26:51 -0700

    final version

--- ass2.js
+++ ass2.js
@@ -1,104 +1,177 @@
 
 var user=[];
+var error='<p  id="error1"> please fill the username</p>';
+
 if(localStorage.a_users)
 {
 user=JSON.parse(localStorage.a_users)
 }
  
+
+
+
 var a=document.getElementById("uname")
 var b=document.getElementById("pass")
 var c=document.getElementById("fname")
 var d=document.getElementById("lname")
 var e=document.getElementById("email")
-var f=document.getElementById("gender")
+var f=document.getElementsByName("Gender")
 var g=document.getElementById("location")
 
 
+var a1=1;
+ var a2=1;var a3=1;var a4=1;var a5=1;var a6=1;var a7=1;
 
-var a1=1; var a2=1;var a3=1;var a4=1;var a5=1;var a6=1;var a7=1;
+
 document.getElementById("save").addEventListener("click",function()
 
 {
+    
+    if (a.value =="") 
+    {
+        
 
+        if(document.getElementById("error1")==null)
+        {
+            a.insertAdjacentHTML("afterend",' <p style="color:red", id="error1"> please fill the uname</p>');  
+        }
+        
+ }
+ 
+                 else if(a.value !=""){  
+
+        if(document.getElementById("error1")!=null){
+            document.getElementById("error1").remove();
+        }
+    }
+
+  
+
+   if (b.value.length ==0) 
+    {
+        
+
+        if(document.getElementById("error2")==null)
+        {
+            b.insertAdjacentHTML("afterend",' <p style="color:red", id="error2"> please fill the pass</p>');  
+        }
+        
+ }
+
+                 else if(b.value.length !=0){  
+
+        if(document.getElementById("error2")!=null){
+            document.getElementById("error2").remove();
+        }
+    }
+
+       if (c.value.length ==0) 
+    {
+        if(document.getElementById("error3")==null)
+        {
+            c.insertAdjacentHTML("afterend",' <p style="color:red", id="error3"> please fill the firstName</p>');  
+        }
+        
+ }
+ 
+                 else if(c.value.length !=0){  
+
+        if(document.getElementById("error3")!=null){
+            document.getElementById("error3").remove();
+        }
+    }
+      if (d.value.length==0) 
+    {
+        if(document.getElementById("error4")==null)
+        {
+            d.insertAdjacentHTML("afterend",' <p style="color:red", id="error4"> please fill the lastName</p>');  
+        }
+        
+ }
+ 
+                 else if(d.value.length !=0){  
+
+        if(document.getElementById("error4")!=null){
+            document.getElementById("error4").remove();
+        }
+    }
+
+
+
+       if (e.value.length==0) 
+    {
+        if(document.getElementById("error5")==null)
+        {
+            e.insertAdjacentHTML("afterend",' <p style="color:red", id="error5"> please fill the email</p>');  
+        }
+        
+ }
+ 
+                 else if(e.value.length !=0){  
+
+        if(document.getElementById("error5")!=null){
+            document.getElementById("error5").remove();
+        }
+    }
 
  
+if(f[0].checked==false && f[1].checked==false )
+{
+
+    if(document.getElementById("error6")==null)
+        {
+            g.insertAdjacentHTML("beforeBegin",' <p style="color:red", id="error6"> please fill the gender</p>');  
+        }
+        
+ }
  
-    if (a.value === "" &&  a1==1) 
-    {
-    
-       a.insertAdjacentHTML("afterend", " <p style=color:red> please fill the username</p> ");
-       
-       a1++;
-       
-     
+                 else if(f[0].checked==true || f[1].checked==true ){  
 
-    }
-
-      
-
-       
-    
-
-
-    
-  if (b.value === "" &&  a2==1) 
-    {
-        b.insertAdjacentHTML("afterend","<p style=color:red>password must be filled</p>")
-        a2++;
-    } 
-
-       if (c.value ==="" &&  a3==1) 
-    {
-        c.insertAdjacentHTML("afterend","<p style=color:red>FirstName must be filled</p>")
-        a3++;
-    }
-      if (d.value ==="" &&  a4==1) 
-    {
-        d.insertAdjacentHTML("afterend","<p style=color:red>LastName must be filled</p>")
-        a4++;
-    }
-       if (e.value ==="" &&  a5==1) 
-    {
-        e.insertAdjacentHTML("afterend","<p style=color:red>email must be filled</p>")
-        a5++;
+        if(document.getElementById("error6")!=null){
+            document.getElementById("error6").remove();
+        }
     }
 
 
 
-   
 
+
+
+     if (g.value.length==0) 
+    {
+        if(document.getElementById("error7")==null)
+        {
+            g.insertAdjacentHTML("afterend",' <p style="color:red", id="error7"> please fill the location</p>');  
+        }
         
-       
-    
-    if (f.value ==="" &&  a6==1) 
-    {
-        f.insertAdjacentHTML("afterend","<p style=color:red>Gender must be filled</p>")
-        a6++;
+ }
+ 
+                 else if(g.value.length !=0){  
+
+        if(document.getElementById("error7")!=null){
+            document.getElementById("error7").remove();
+        }
     }
 
 
-
-
-    
-       if (g.value ==="" &&  a7==1) 
-    {
-        g.insertAdjacentHTML("afterend","<p style=color:red>location must be filled</p>")
-        a7++;
-        
-    }
-
-
-        var radios = document.getElementsByName('genderS');
 
 
     
   
-if(a.value!=="" && b.value!=="" && c.value!=="" && d.value!=="" && e.value!=="" && f.value!=="" && g.value!=="")
+if(a.value!=="" && b.value!=="" && c.value!=="" && d.value!=="" && e.value!==""  &&  f[0].checked==true || f[1].checked==true  &&   g.value!=="")
 
 {
 
-
-
+    var zx=function()
+    {
+if(f[0].checked==true)
+{
+return "male"
+}
+else{
+    return "female"
+}
+    }
 
 
     var users={
@@ -108,7 +181,7 @@ if(a.value!=="" && b.value!=="" && c.value!=="" && d.value!=="" && e.value!=="" 
         fname:c.value,
         lname:d.value,
         email:e.value,
-        gender:f.value,
+       Gender: zx(),
         location:g.value
     
     }
@@ -139,10 +212,9 @@ for(var i=0;i<user.length;i++)
     cell3.innerHTML=user[i].fname;
     cell4.innerHTML=user[i].lname;
     cell5.innerHTML=user[i].email;
-    cell6.innerHTML=user[i].gender;
+    cell6.innerHTML=user[i].Gender;
     cell7.innerHTML=user[i].location;
-}
 }
 
 }
-)
+})
